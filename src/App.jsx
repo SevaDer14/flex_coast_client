8e133db00ccb0a3f8ac61c8b4db678380dc144f3
lucas <laksteelhouse@gmail.com>
2021-06-04 22:14:38 +0200

    Sets up folder structure and scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import './styles/desktop.css'
+import './styles/globals.css'
 
 const App = () => {
   const { t } = useTranslation()
   return (
     <>
-      <h1>{t('getStarted')}</h1>
+      <h1 className='title'>{t('getStarted')}</h1>
     </>
   )
 }
