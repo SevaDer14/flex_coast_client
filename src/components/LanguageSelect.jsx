493d65a031726bbfbdc798eb067d331b00f5bf43
arunbhalli <tomarbhalli@gmail.com>
2021-06-05 16:58:33 +0200

    Add style and modified language drop down menu

--- src/components/LanguageSelect.jsx
+++ src/components/LanguageSelect.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
 import i18n from '../i18n'
 
 const LanguageSelect = () => {
+  const [language,setLanguage]= useState('English')
   const handleChange = (event) => {
+    setLanguage(event.target.value)
     i18n.changeLanguage(event.target.value)
   }
   return (
     <div>
       <Select
+      style={styles.menu}
         data-cy='language-dropdown'
         inputProps={{
           name: 'language',
           id: 'age-native-simple',
         }}
+       
+        value={language}
+        displayEmpty
         onChange={handleChange}>
-        <option aria-label='None' value='' />
-        <option data-cy='english' value={'en'}>English</option>
-        <option data-cy='svenska' value={'se'}>Svenska</option>
+        <MenuItem data-cy='english' value={'English'}>
+          English
+        </MenuItem>
+        <MenuItem data-cy='svenska' value={'Svenska'}>
+          Svenska
+        </MenuItem>
       </Select>
     </div>
   )
 }
 
 export default LanguageSelect
+
+const styles = {
+menu:{ 
+  textAlign: 'right',
+  color: 'white',
+  textDecorationColor:"white"
+}
+}
