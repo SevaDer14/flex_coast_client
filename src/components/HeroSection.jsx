4640496269a8f947ec0c0ffe5a71a7e68b32edec
luke <lukeperrin27@gmail.com>
2021-06-08 20:47:50 +0200

    adds translations for landing page update to styling of language select

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import CustomButton from '../custom/CustomButton'
 import LandingHeader from './LandingHeader'
+import { useTranslation } from 'react-i18next'
 
 const HeroSection = () => {
+  const { t } = useTranslation()
   return (
     <div className='hero-section' data-cy='hero-section'>
       <LandingHeader />
       <div data-cy='welcome-text' className='welcome-text'>
-        <h1>Flexible short term office leasing in lovely Gothenburg</h1>
+        <h1>{t('landingPage.introText')}</h1>
         <CustomButton dataCy='get-started' link to='/inquiry'>
-          Get Started
+          {t('landingPage.startButton')}
         </CustomButton>
       </div>
     </div>
--- src/locales/se.js
+++ src/locales/se.js
@@ -1,19 +1,22 @@
 const se = {
   translation: {
-    getStarted: 'Hitta kontor',
-    title: 'Flex Coast',
+    landingPage: {
+      introText: 'Flexibel korttidsuthyrning i vackra Göteborg',
+      startButton: 'Hitta Kontor',
+    },
+
     welcomeMessage:
-    "Hej! För att att hitta bästa möjliga kontor, behöver vi ställa lite frågor.",
+      'Hej! För att att hitta bästa möjliga kontor, behöver vi ställa lite frågor.',
 
-  question: {
-    email: 'Hur kan vi kontakta dig?',
-  },
+    question: {
+      email: 'Hur kan vi kontakta dig?',
+    },
 
-  answer: {
-    email: 'Min email är: ',
+    answer: {
+      email: 'Min email är: ',
+    },
+    submitButton: 'Skicka in',
   },
-  submitButton: 'Skicka in',
-  }
 }
 
 export default se
