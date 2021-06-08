b5e06bde6f7c0d7e7a17ee16b626f6fd77e9c0c3
sevader14 <vsevolod.deriushkin@gmail.com>
2021-06-08 21:25:14 +0200

    fixes tests after merge

--- cypress/integration/userCanEditTheirInquiry.feature.js
+++ cypress/integration/userCanEditTheirInquiry.feature.js
@@ -1,6 +1,6 @@
 describe('User can edit inquiry', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/inquiry')
   })
   describe('Successfully', () => {
     it('is expected to edit input field', () => {
