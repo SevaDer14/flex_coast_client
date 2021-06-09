a3d4d4ca4d6eda1d26a101356a14a80372183186
DavDan1 <david-danielyan@hotmail.com>
2021-06-09 10:48:31 +0200

    adds build:dev and build:prod to package.json adds default base URL to heroku if dev or prod

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Header from './components/Header'
 import { Route, Switch } from 'react-router-dom'
 import LandingPage from './views/LandingPage'
 
-
 const App = () => {
   return (
     <Switch>
