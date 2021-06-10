51587549a0331fbc6a74b3e92b5efd21270e9ef7
lucas <laksteelhouse@gmail.com>
2021-06-10 11:48:59 +0200

    Adds testimonials title

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,47 +1,27 @@
-import React, { useState } from 'react'
-import companyLogo from '../assets/logo.png'
+import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import Divider from '@material-ui/core/Divider'
 
-
-const TestimonialSlider = () => {
-  const [slider, setSlider] = useState(1)
-  const sliderHandler = (number) => {
-    setSlider(slider + number)
-    if (slider + number === 4) {
-      setSlider(1)
-    } else if (slider + number === 0) {
-      setSlider(3)
-    }
-  }
+const TestimonialSlider = ({ slider, info, sliderHandler }) => {
   return (
     <div className={`slider-container bg-${slider}`}>
       <div className='testimonial-container'>
         <div className={`social-image person-${slider}`}></div>
         <div className='info-container'>
-          <h4>
-            "Testimonial Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Placeat nihil quo nostrum rem. Officia cupiditate repellendus,
-            saepe rem deleniti voluptatibus illum aliquid magnam non distinctio
-            quam soluta laborum minima facilis."
-          </h4>
+          <h4>{info.review}</h4>
           <Divider light />
           <div>
-            <p className='name'>Johnny Cage</p>
-            <p className='job-title'>CEO of MK</p>
+            <p className='name'>{info.name}</p>
+            <p className='job-title'>{info.company}</p>
           </div>
 
           <div className='logo-section'>
             <IconButton onClick={() => sliderHandler(-1)}>
               <ChevronLeftIcon fontSize='large' />
             </IconButton>
-            <img
-              className='company-logo'
-              src={companyLogo}
-              alt='company-logo'
-            />
+            <img className='company-logo' src={info.logo} alt='company-logo' />
             <IconButton onClick={() => sliderHandler(1)}>
               <ChevronRightIcon fontSize='large' />
             </IconButton>
