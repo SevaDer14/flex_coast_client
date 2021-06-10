e274bfda56a79a90775779c379bf6c32ca499c4c
Thomas <thomas@craftacademy.se>
2021-06-11 00:31:07 +0200

    WIP adds markers to map adds silver styling to map

--- src/components/MapSection.jsx
+++ src/components/MapSection.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import { providers } from '../data/gothenburg'
+import { mapStyles } from '../data/mapStyles'
 
 const MapSection = () => {
   let center = {
@@ -12,19 +14,46 @@ const MapSection = () => {
     height: '600px',
   }
 
+  const defaultMapOptions = {
+    fullScreenControl: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    styles: mapStyles,
+  }
+
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_KEY,
   })
+  let markers = providers.map((provider) => {
+    return (
+      <Marker
+        key={providers.name}
+        icon={{
+          path: 'M182.9,551.7c0,0.1,0.2,0.3,0.2,0.3S358.3,283,358.3,194.6c0-130.1-88.8-186.7-175.4-186.9 C96.3,7.9,7.5,64.5,7.5,194.6c0,88.4,175.3,357.4,175.3,357.4S182.9,551.7,182.9,551.7z M122.2,187.2c0-33.6,27.2-60.8,60.8-60.8 c33.6,0,60.8,27.2,60.8,60.8S216.5,248,182.9,248C149.4,248,122.2,220.8,122.2,187.2z',
+          scale: 0.09,
+          strokeColor: 'darkgrey',
+          strokeWeight: 2,
+        }}
+        position={{
+          lat: provider.fullAddress.latitude,
+          lng: provider.fullAddress.longitude,
+        }}
+      />
+    )
+  })
 
   return (
     isLoaded && (
-      <div className='map'>
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={15}></GoogleMap>
-      </div>
+      <GoogleMap
+        options={defaultMapOptions}
+        className='map'
+
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={14}>
+        {markers}
+      </GoogleMap>
     )
   )
 }
---
+++ src/data/gothenburg.js
@@ -0,0 +1,310 @@
+export const providers = [
+  {
+    name: 'United Spaces',
+    address: 'Östra Hamngatan 16, Nordstaden',
+    description:
+      'Unites Spaces i Göteborg ligger på Östra Hamngatan 16 nära centralstationen. Femstjärnigt läge, med andra ord! Mycket som du annars hade behövt lägga pengar och tid på ingår i priset. Då tänker vi på allt från kaffe till kontorsmöbler. Som hyresgäst på United Spaces Göteborg kan du även hyra fullt utrustade mötesrum till väldigt bra pris.',
+    fullAddress: {
+      streetName: 'Östra Hamngatan',
+      postCode: '41109',
+      latitude: 57.7069738,
+      longitude: 11.9680455,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/18041?height=1800&width=3200&fit=crop&updatedDate=1583424503',
+      'https://workaround.io/api/asset/image/18042?height=1800&width=3200&fit=crop&updatedDate=1583424503',
+    ],
+  },
+  {
+    name: 'Regus',
+    address: 'Östra Hamngatan 17, Nordstaden',
+    description:
+      'Fräscht, modernt center med en stor härlig lounge längst upp i fastigheten på plan 5. Här har vi kontor i olika storlekar och  en stor härlig co-workmiljö.',
+    fullAddress: {
+      streetName: 'Östra Hamngatan',
+      postCode: '41109',
+      latitude: 57.7069738,
+      longitude: 11.9680455,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/16651?height=1800&width=3200&fit=crop&updatedDate=1583424488',
+      'https://workaround.io/api/asset/image/16652?height=1800&width=3200&fit=crop&updatedDate=1583424488',
+    ],
+  },
+  {
+    name: 'United Spaces',
+    address: 'Pumpgatan 3, Lundby',
+    description:
+      'Den första september öppnar vi äntligen upp portarna till UNITED SPACES Lindholmen- en fantastisk coworking-arena i Göteborgs mest dynamiska och innovativa område - Uni3 by GeelyÄr du på jakt efter en arbetsplats präglad av flexibilitet, öppenhet och gemenskap? Vill du vara del av ett koncept där människor möts, idéer frodas och affärer skapas? Och där någon annan tar hand om allt det praktiska medan du jobbar.Ta del av vårt ’Early Bird-erbjudande’ med upp till 25% rabatt de första 6 månaderna',
+    fullAddress: {
+      streetName: 'Pumpgatan',
+      streetNumber: '3',
+      postCode: '41755',
+      latitude: 57.7117951,
+      longitude: 11.9456073681949,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/25224?height=1800&width=3200&fit=crop&updatedDate=1622623039',
+      'https://workaround.io/api/asset/image/25225?height=1800&width=3200&fit=crop&updatedDate=1622623114',
+    ],
+  },
+  {
+    name: 'Arkipelagen',
+    address: 'Järnvågsgatan 3, Olivedal',
+    description:
+      'Välkomna till Arkipelagen Järntorget! Här har vi ca 40 kontorsrum, tre konferensrum och ett härligt kök där vi serverar frukost varje vardag. Ett bra tillfälle att knyta kontakt med andra hyresgäster eller under lunchen. I vår härliga lounge kan du både arbeta och ta en kaffe med dina gäster, eller bara koppla av en stund. Du tar dig enkelt till oss med kollektivtrafik, cykel eller bil. Parkeringshus Snipan ligger precis intill.',
+    fullAddress: {
+      streetName: 'Järnvågsgatan',
+      postCode: '41303',
+      latitude: 57.7012035,
+      longitude: 11.9509537,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/23326?height=1800&width=3200&fit=crop&updatedDate=1615813730',
+      'https://workaround.io/api/asset/image/23329?height=1800&width=3200&fit=crop&updatedDate=1615813730',
+    ],
+  },
+  {
+    name: 'Spaces Brygghuset Lindholmen',
+    address: 'Theres Svenssons gata 13, Lundby',
+    description:
+      'Nybyggda kontorslokaler med högt i tak, panormafönster och stor terrass med havsutsikt.  Spaces Brygghuset är utrustad med passersystem och larm. Privata kontorsrum vån 1 & 2,  i storlekar 6-200 kvm. Många kontorsrum erbjuder vacker havsutsikt. Telefonrum, badrum med duschmöjligheter och kök på varje våningsplan. I vår Business Club i entréplan hittar du också vårt trevliga café.  Toppmodern och kreativ Coworkmiljö -  välj mellan flexibel plats eller egen fast plats. Valfri avtalsperiod.',
+    fullAddress: {
+      streetName: 'Theres Svenssons Gata',
+      streetNumber: '13',
+      postCode: '41755',
+      latitude: 57.7066147,
+      longitude: 11.9439541,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/13999?height=1800&width=3200&fit=crop&updatedDate=1583424462',
+      'https://workaround.io/api/asset/image/13989?height=1800&width=3200&fit=crop&updatedDate=1583424462',
+    ],
+  },
+  {
+    name: 'Anders Carlssons gata 14',
+    address: 'Lundby',
+    description:
+      'Vi är ett personligt kontorshotell, centralt beläget på attraktiva Lindholmen, med fokus på trivsel, gemenskap och flexibla lösningar. Vi erbjuder ett trevligt, ombonat kontor med stabila IT-lösningar, härlig utsikt och gott kaffe! Det är enkelt att hitta parkeringsplats i anslutning till kontoret men vill du åka kollektivt så är det nära också! Här finns även möjlighet till omklädningsskåp och dusch. Många attraktiva företag och restauranger ligger inom fem minuters promenad-avstånd.',
+    fullAddress: {
+      postCode: '44296',
+      latitude: 57.9267894,
+      longitude: 11.9011002,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/15382?height=1800&width=3200&fit=crop&updatedDate=1583424475',
+      'https://workaround.io/api/asset/image/15383?height=1800&width=3200&fit=crop&updatedDate=1583424475',
+    ],
+  },
+  {
+    name: 'Ringögatan 6',
+    address: 'Lundby',
+    description:
+      'Välkommen till ett litet och personligt kontorshotell. Ringöns kontorshotell ligger vid Ringögatan nära Götaälvbron. Läget är fördelaktigt på många sätt, Ringön är ett expansivt område där det kommer att hända mycket under de närmaste åren. Man tar sig lätt ut på alla större leder och in till centrala Göteborg på ett par minuter. Vill man ta sig hit kollektivt finns det både buss och spårvagn intill men här finns också fri parkering samt möjlighet att hyra garageplats. Vi har ca 15 rum med varierande storlek, mellan 8-30 m2. Du väljer själv om du vill hyra kontoret omöblerat eller möblerat. ​Alla rum är ljusa och moderna med nyinstallerad luftkonditionering. Som hyresgäst har du tillgång till ditt kontor och våra gemensamma utrymmen dygnet runt alla dagar på året.',
+    fullAddress: {
+      postCode: '44296',
+      latitude: 57.9267894,
+      longitude: 11.9011002,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/16546?height=1800&width=3200&fit=crop&updatedDate=1583424487',
+      'https://workaround.io/api/asset/image/16547?height=1800&width=3200&fit=crop&updatedDate=1583424487',
+    ],
+  },
+  {
+    name: 'Första Långgatan 22',
+    address: 'Olivedal',
+    description:
+      'Ljust och luftigt kontorsrum på sjätte våningen uthyres. Lokalen har ett rymligt konferensrum och ett modernt kök samt ett större badrum med dusch och ett WC. I lokalen finns 4 ytterligare kontorsrum samt  3 öppna arbetsplatser. Rummen avdelas med glasväggar vilket gör att utrymmena uppfattas som ljusa och rymliga.',
+    fullAddress: {
+      latitude: 57.6936562,
+      longitude: 11.9489129,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/22577?height=1800&width=3200&fit=crop&updatedDate=1615813729',
+      'https://workaround.io/api/asset/image/22576?height=1800&width=3200&fit=crop&updatedDate=1615813729',
+    ],
+  },
+  {
+    name: 'Järntorget 3',
+    address: 'Olivedal',
+    description:
+      'Välkommen till Järntorget 3 där det nu finns en fantastisk lokal på 150kvm att hyra till flexibla villkor! Lokalen är uppdelad i 5 rum varav 2 kontorsrum, två mötesrum och ett kök. Passar perfekt för företaget på 8-12 personer som vill sitta i en representativ lokal nära stadskärnan. Möjlighet finns till tidigare inflytt. Varmt välkommen på visning!',
+    fullAddress: {
+      latitude: 57.6936562,
+      longitude: 11.9489129,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/8398?height=1800&width=3200&fit=crop&updatedDate=1583424408',
+      'https://workaround.io/api/asset/image/8394?height=1800&width=3200&fit=crop&updatedDate=1583424408',
+    ],
+  },
+  {
+    name: 'Tokali',
+    address: 'Kyrkogatan 30, Inom Vallgraven',
+    description: 'Ljust och fräscht kontor.',
+    fullAddress: {
+      streetName: 'Kyrkogatan',
+      streetNumber: '30',
+      postCode: '41115',
+      latitude: 57.7049131,
+      longitude: 11.9656043,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/12179?height=1800&width=3200&fit=crop&updatedDate=1583424445',
+      'https://workaround.io/api/asset/image/12175?height=1800&width=3200&fit=crop&updatedDate=1583424445',
+    ],
+  },
+  {
+    name: 'Korsgatan 3',
+    address: 'Inom Vallgraven',
+    description:
+      'Kontoret är på 147 m2. Vi använder 3 rum och en kontorsplats själva och hyr ut 3 rum. En av hyresgästerna, en försäkringsmäklare, har hyrt hos oss i 15 år. En annan hyresgäst, en advokat, flyttar efter att ha varit hos oss i flera år.Vi vill hyra ut ett kontorsrum med tillgång till vårt jättefina konferensrum. Erbjudandet är lämpligt för t.ex. en advokat, personalrekryterare eller överhuvudtaget för någon som har många besökare. Passar perfekt dig som inte vill sitta i ett öppet landskap!',
+    fullAddress: {
+      latitude: 57.7042775,
+      longitude: 11.9645697,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/18889?height=1800&width=3200&fit=crop&updatedDate=1615813722',
+      'https://workaround.io/api/asset/image/18802?height=1800&width=3200&fit=crop&updatedDate=1615813722',
+    ],
+  },
+  {
+    name: 'Stora Nygatan 13',
+    address: 'Inom Vallgraven',
+    description:
+      'Söker du en hel kontorslokal eller en fast eller flexibel kontorsplats i centrala Göteborg? Ett nytt kontorshotell på 300 m2 slår upp dörrarna på Stora Nygatan 13. Lokalen är ljus med parkettgolv och högt i tak och det finns en balkong som överblickar vallgraven och Trädgårdsföreningen, samt en trevlig lounge, matsal och två mindre möteslokaler. Det går att välja mellan en eller flera kontorsplatser i ett eget rum eller i biblioteket, som har öppen planlösning.',
+    fullAddress: {
+      latitude: 57.7042775,
+      longitude: 11.9645697,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/23462?height=1800&width=3200&fit=crop&updatedDate=1615813730',
+      'https://workaround.io/api/asset/image/23464?height=1800&width=3200&fit=crop&updatedDate=1615813730',
+    ],
+  },
+  {
+    name: 'Dockside Office & Coworking',
+    address: 'Regnbågsgatan 8B, Lundby',
+    description:
+      'Hos Dockside Office & Coworking hittar du allt från traditionella kontorsrum till öppen coworking space. Alltsammans på bästa läge på Lindholmen - Göteborgs innovationshubb nummer ett. Med Dockside Desk får du samma förmåner som våra kontorsrumskunder, utan att du behöver hyra ett eget rum. Du får tillträde till lokalen dygnet runt och tillgång till gemensamma ytor som lunchrum, konferens- och mötesrum, Hos oss samlas företag från olika branscher som ger nya nätverks- och affärsmöjligheter. Du hittar oss i det anrika Gothiahuset på Regnbågsgatan 8B och når oss enkelt med såväl bil som buss eller båt som erbjuder täta förbindelser till city och centralstation.  Välkommen på visning!',
+    fullAddress: {
+      streetName: 'Regnbågsgatan',
+      streetNumber: '8B',
+      postCode: '41755',
+      latitude: 57.7104825,
+      longitude: 11.94537,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/18106?height=1800&width=3200&fit=crop&updatedDate=1583424503',
+      'https://workaround.io/api/asset/image/18107?height=1800&width=3200&fit=crop&updatedDate=1583424503',
+    ],
+  },
+  {
+    name: 'Entreprenörsgatan',
+    address: 'Ingenjörsgatan 8, Inom Vallgraven',
+    description:
+      'På den här attraktiva adressen finns det 12 fasta kontorsplatser varav fyra i eget rum och fyra flexibla arbetsplatser. De företag som sitter här idag har stort fokus inom de digtala området men kontoret har en hemtrevlig och proffesionell miljö som passar de flesta företag. Här får du inte bara en arbetsplats med tillgång till allt du behöver, du får även ett härligt nätverk av grymma företag och entreprenörer från alla våra sju lokaler!',
+    fullAddress: {
+      streetName: 'Ingenjörsgatan',
+      streetNumber: '8',
+      postCode: '411 13',
+      latitude: 57.7031867,
+      longitude: 11.9571321,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/15213?height=1800&width=3200&fit=crop&updatedDate=1583424474',
+      'https://workaround.io/api/asset/image/15208?height=1800&width=3200&fit=crop&updatedDate=1583424474',
+    ],
+  },
+  {
+    name: 'Entreprenörsgatan',
+    address: 'Kungsgatan 4, Inom Vallgraven',
+    description:
+      'På Entreprenörsgatan erbjuder vi både coworkingspace och kontorsrum i centrala Göteborg. På 550m2 skapar flera företag ett fantastiskt nätverk tillsammans från flera olika branscher. De inglasade kontorsrummen är färdigmöblerade med eget höj- och sänkbart skrivbord, kontorsstol och andra kontorstillbehör. Flexplatserna finns i den öppna loungen och vid skrivborden med skärm som finns att låna. Kontoret är utrustat med kök, WC, lounge, konferensrum, skrivare och WiFi.',
+    fullAddress: {
+      streetName: 'Kungsgatan',
+      postCode: '411 19',
+      latitude: 57.7030712,
+      longitude: 11.9590075,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/24915?height=1800&width=3200&fit=crop&updatedDate=1618298820',
+      'https://workaround.io/api/asset/image/24917?height=1800&width=3200&fit=crop&updatedDate=1618298822',
+    ],
+  },
+  {
+    name: 'Drottninggatan 38',
+    address: 'Inom Vallgraven',
+    description:
+      'Lokalen är stilren med stora, öppna ytor och dämpad akustik. Som en del av Codic Coworking har du även tillgång till vårt kontor vid Trädgårdsföreningen. Samtliga skrivbordsplatser har höj- och sänkbara skrivbord, ergonomiska kontorsstolar samt tillgång till låsbart skåp och konferens- och mötesrum i mån av plats. Det finns även ett avslappningsrum, telefonbås och ett trevligt lunchrum med soffgrupper och fåtöljer.',
+    fullAddress: {
+      latitude: 57.7042775,
+      longitude: 11.9645697,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/23372?height=1800&width=3200&fit=crop&updatedDate=1615813730',
+      'https://workaround.io/api/asset/image/24588?height=1800&width=3200&fit=crop&updatedDate=1615813733',
+    ],
+  },
+  {
+    name: 'Nordenskiöldsgatan 14',
+    address: 'Olivedal',
+    description:
+      'Ett härligt kontor på andra våningen. Kontoret består av tre rum och gemensamma ytor som förråd, kök och toalett med dusch. Kontorsrummet som annonseras är det största rummet som idag huserar höj/sänkbara bord och stolar för 4 personer samt loungeavdelning. I det fullt utrustade köket finns stort bord för lunch och en skön soffa för avslappning. Vi som sitter här idag - era framtida kontorsgrannar - har en mycket god och härlig relation och kontoret inbjuder till utbyten och varma möten.',
+    fullAddress: {
+      latitude: 57.6936562,
+      longitude: 11.9489129,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/22574?height=1800&width=3200&fit=crop&updatedDate=1615813729',
+      'https://workaround.io/api/asset/image/22571?height=1800&width=3200&fit=crop&updatedDate=1615813729',
+    ],
+  },
+  {
+    heading: 'Kolgruvegatan 12',
+    address: 'Lundby',
+    description:
+      'Våra kontor är alla unika i både storlek och design. Här kan ni jobba ostört och kreativt.  Oavsett om ni har ett kontor eller ej så kommer ni alltid att tillhöra en öppen och kollaborativ miljö med tillgång till allt vad vi kan erbjuda.  Välkommen på visning!  Villkor:  Vi skriver tillsvidarekontrakt med två månaders uppsägningstid eller bundet på minst sex månader. Vid bundet kontrakt är första månaden alltid gratis.  Rekommenderar man en vän som tar plats i det öppna landskapet får man själv en månad gratis.  Vi tar en deposition på två månadshyror oavsett kontrakt.',
+    fullAddress: {
+      postCode: '44296',
+      latitude: 57.9267894,
+      longitude: 11.9011002,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/17770?height=1800&width=3200&fit=crop&updatedDate=1583424500',
+      'https://workaround.io/api/asset/image/17771?height=1800&width=3200&fit=crop&updatedDate=1583424500',
+    ],
+  },
+  {
+    heading: 'Entreprenörsgatan',
+    address: 'Östra Hamngatan 1, Nordstaden',
+    description:
+      'Välkommen till detta fantastiska kontorshotell. Här möter Göteborgs industrihistoria dagens entreprenörer och skapar en härlig kontorsmiljö med fina detaljer från nyrenässansstilen, högt i tak och utsikt över operan. De inglasade kontorsrummen är möblerade med eget höj- och sänkbart skrivbord, kontorsstol och andra kontorstillbehör. För flexplatserna finns det skärm och skrivbordsplats att låna.  Kontoret är även utrustat med kök, WC, lounge, konferensrum, skrivare och WiFi.',
+    fullAddress: {
+      streetName: 'Östra Hamngatan',
+      streetNumber: '1',
+      postCode: '41110',
+      latitude: 57.7097802,
+      longitude: 11.966166,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/24937?height=1800&width=3200&fit=crop&updatedDate=1618308499',
+      'https://workaround.io/api/asset/image/24942?height=1800&width=3200&fit=crop&updatedDate=1618308531',
+    ],
+  },
+  {
+    heading: 'Lasarettsgatan 9',
+    address: 'Inom Vallgraven',
+    description:
+      'Vi vill dela med oss av vår fina, nyrenoverade kontorslokal och söker kontorsgäster som är intresserade av att sitta på en av Göteborgs bästa adresser. Ni har tillgång till konferensrum med vacker utsikt över Göta Älv, fritt fiber och en positiv atmosfär. 1-2 platser finns till förfogande.  Du kommer ha nära både kommunikationer och bra afterwork.  Kontraktstid löper på perioder om 3 månader löpande och ni kan flytta in omgående.',
+    fullAddress: {
+      latitude: 57.7042775,
+      longitude: 11.9645697,
+    },
+    images: [
+      'https://workaround.io/api/asset/image/17653?height=1800&width=3200&fit=crop&updatedDate=1583424499',
+      'https://workaround.io/api/asset/image/17654?height=1800&width=3200&fit=crop&updatedDate=1583424499',
+    ],
+  },
+]
