fd0f1ac8b1dd32a05db6731bdb5ea5fd053e0978
lucas <laksteelhouse@gmail.com>
2021-06-10 18:41:57 +0200

    Adds framer motion, changes param key for create action

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ import Grid from '@material-ui/core/Grid'
 const Footer = () => {
   return (
     <>
-      <Grid data-cy='footer' id='footer' container alignItems='center' spacing={3}>
+      <Grid data-cy='footer' id='footer' container alignItems='center'>
         <Grid className='footer-header' item xs={12}>
           <h1>Flex Coast</h1>
           <hr className='black-br' />
         </Grid>
-        <Grid data-cy='footer-categories' container alignItems='flex-start' spacing={3}>
-          <Grid data-cy='footer-about' className='sub-header' item xs={12} sm={4}>
+        <Grid data-cy='footer-categories' container alignItems='flex-start'>
+          <Grid data-cy='footer-about' className='sub-header' item sm={4}>
             <h3>About</h3>
             <p className='footer-text'>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
@@ -33,7 +33,12 @@ const Footer = () => {
               <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>link</a>
             </p>
           </Grid>
-          <Grid data-cy='footer-contact' className='sub-header' item xs={12} sm={4}>
+          <Grid
+            data-cy='footer-contact'
+            className='sub-header'
+            item
+            xs={12}
+            sm={4}>
             <h3>Contact</h3>
             <p className='footer-text'>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,32 +3,45 @@ import IconButton from '@material-ui/core/IconButton'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import Divider from '@material-ui/core/Divider'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const TestimonialSlider = ({ slider, info, sliderHandler }) => {
   return (
-    <div className={`slider-container bg-${slider}`}>
-      <div className='testimonial-container'>
-        <div className={`social-image person-${slider}`}></div>
-        <div className='info-container'>
-          <h4>{info.review}</h4>
-          <Divider light />
-          <div>
-            <p className='name'>{info.name}</p>
-            <p className='job-title'>{info.company}</p>
-          </div>
+    <AnimatePresence initial={false}>
+      <motion.div
+        className={`slider-container bg-${slider}`}
+        key={slider}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }}>
+        <div className='testimonial-container'>
+          <div className={`social-image person-${slider}`}></div>
+          <div className='info-container'>
+            <h4>{info.review}</h4>
+            <Divider light />
+            <div>
+              <p className='name'>{info.name}</p>
+              <p className='job-title'>{info.company}</p>
+            </div>
 
-          <div className='logo-section'>
-            <IconButton onClick={() => sliderHandler(-1)}>
-              <ChevronLeftIcon fontSize='large' />
-            </IconButton>
-            <img className='company-logo' src={info.logo} alt='company-logo' />
-            <IconButton onClick={() => sliderHandler(1)}>
-              <ChevronRightIcon fontSize='large' />
-            </IconButton>
+            <div className='logo-section'>
+              <IconButton onClick={() => sliderHandler(-1)}>
+                <ChevronLeftIcon fontSize='large' />
+              </IconButton>
+              <img
+                className='company-logo'
+                src={info.logo}
+                alt='company-logo'
+              />
+              <IconButton onClick={() => sliderHandler(1)}>
+                <ChevronRightIcon fontSize='large' />
+              </IconButton>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 export default TestimonialSlider
