04e917a0aeb12013f55bbb36f0967ed810cb2a91
sevader14 <vsevolod.deriushkin@gmail.com>
2021-06-10 11:32:34 +0200

    wrapps map in a div

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import TestimonialsSection from '../components/TestimonialsSection'
-import MapSection from '../components/MapSection';
+import MapSection from '../components/MapSection'
 
 const LandingPage = () => {
   return (
     <>
       <HeroSection />
       <TestimonialsSection />
-      <MapSection/>
+      <MapSection />
     </>
   )
 }
