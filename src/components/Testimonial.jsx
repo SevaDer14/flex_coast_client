e283c6bafb4954c8534c2b1fa6b1960515fc44c9
luke <lukeperrin27@gmail.com>
2021-06-10 10:57:17 +0200

    styles slideshow button

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import companyLogo from '../assets/logo.png'
+import IconButton from '@material-ui/core/IconButton'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 
 const TestimonialSlider = () => {
   const [slider, setSlider] = useState(1)
@@ -22,18 +25,24 @@ const TestimonialSlider = () => {
             saepe rem deleniti voluptatibus illum aliquid magnam non distinctio
             quam soluta laborum minima facilis."
           </h4>
+
           <div>
             <p className='name'>Johnny Cage</p>
             <p className='job-title'>CEO of MK</p>
-            <div className='logo-section'>
-              <button onClick={() => sliderHandler(-1)}>Prev</button>
-              <img
-                className='company-logo'
-                src={companyLogo}
-                alt='company-logo'
-              />
-              <button onClick={() => sliderHandler(1)}>Next</button>
-            </div>
+          </div>
+
+          <div className='logo-section'>
+            <IconButton onClick={() => sliderHandler(-1)}>
+              <ChevronLeftIcon fontSize='large' />
+            </IconButton>
+            <img
+              className='company-logo'
+              src={companyLogo}
+              alt='company-logo'
+            />
+            <IconButton onClick={() => sliderHandler(1)}>
+              <ChevronRightIcon fontSize='large' />
+            </IconButton>
           </div>
         </div>
       </div>
