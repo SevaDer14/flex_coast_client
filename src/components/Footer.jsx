1d735b5bdf4bba39b6660b048cedd54e3949ae16
kim_haaga <kim.haaga@live.se>
2021-06-11 21:43:16 +0200

    Add logo, color responsive hr

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import logo from '../assets/logoWhite.svg'
 
 const Footer = () => {
   return (
     <>
       <Grid data-cy='footer' id='footer' container alignItems='center'>
         <Grid className='footer-header' item xs={12}>
-          <h1>Flex Coast</h1>
-          <hr className='black-br' />
+        <img src={logo} alt='logo' className='logo' data-cy='flex-coast' />
+          <hr className='top-hr' />
         </Grid>
         <Grid data-cy='footer-categories' container alignItems='flex-start'>
           <Grid data-cy='footer-about' className='sub-header' item sm={4}>
-            <h3>About</h3>
+          <h3>About Us</h3>
             <p className='footer-text'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
-              modi, exercitationem consectetur nisi eveniet dolorum deserunt
-              delectus assumenda aperiam autem.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Laboriosam neque porro voluptatem nisi dolor debitis deserunt et
+              vitae quam odio?
             </p>
+            <hr className='sub-hr' />
+          </Grid>
+          <Grid
+            data-cy='footer-contact'
+            className='sub-header'
+            item
+            xs={12}
+            sm={4}>
+            <h3>Contact</h3>
+            <p className='footer-text'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Laboriosam neque porro voluptatem nisi dolor debitis deserunt et
+              vitae quam odio?
+            </p>
+            <hr className='sub-hr' />
           </Grid>
           <Grid data-cy='links' className='sub-header' item xs={12} sm={4}>
             <h3>Links</h3>
@@ -31,19 +47,6 @@ const Footer = () => {
             </p>
             <p className='footer-link'>
               <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>link</a>
-            </p>
-          </Grid>
-          <Grid
-            data-cy='footer-contact'
-            className='sub-header'
-            item
-            xs={12}
-            sm={4}>
-            <h3>Contact</h3>
-            <p className='footer-text'>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Laboriosam neque porro voluptatem nisi dolor debitis deserunt et
-              vitae quam odio?
             </p>
           </Grid>
         </Grid>
