1b0ff3f48042dbc7dcff18369956e629342a93ad
Christer <christerforsgren91@gmail.com>
2021-06-11 22:42:36 +0200

    Add phone button and get started button to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import logo from '../assets/logoWhite.svg'
+import CustomButton from './custom/CustomButton'
+import { useTranslation } from 'react-i18next'
 
 const Footer = () => {
+  const { t } = useTranslation()
   return (
     <>
       <Grid data-cy='footer' id='footer' container alignItems='center'>
         <Grid className='footer-header' item xs={12}>
-        <img src={logo} alt='logo' className='logo' data-cy='flex-coast' />
+          <img src={logo} alt='logo' className='logo' data-cy='flex-coast' />
           <hr className='top-hr' />
         </Grid>
         <Grid data-cy='footer-categories' container alignItems='flex-start'>
           <Grid data-cy='footer-about' className='sub-header' item sm={4}>
-          <h3>About Us</h3>
+            <h3>About Us</h3>
             <p className='footer-text'>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Laboriosam neque porro voluptatem nisi dolor debitis deserunt et
@@ -27,27 +30,17 @@ const Footer = () => {
             xs={12}
             sm={4}>
             <h3>Contact</h3>
-            <p className='footer-text'>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Laboriosam neque porro voluptatem nisi dolor debitis deserunt et
-              vitae quam odio?
-            </p>
+            <CustomButton dataCy='phone-number'>
+              <a href='tel:+46812345678'>08-123 456 78</a>
+            </CustomButton>
+            <CustomButton dataCy='get-started' link to='/inquiry'>
+              {t('landingPage.startButton')}
+            </CustomButton>
+
             <hr className='sub-hr' />
           </Grid>
           <Grid data-cy='links' className='sub-header' item xs={12} sm={4}>
-            <h3>Links</h3>
-            <p className='footer-link'>
-              <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>link</a>
-            </p>
-            <p className='footer-link'>
-              <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>link</a>
-            </p>
-            <p className='footer-link'>
-              <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>link</a>
-            </p>
-            <p className='footer-link'>
-              <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>link</a>
-            </p>
+            <h3>Find Us</h3>
           </Grid>
         </Grid>
       </Grid>
