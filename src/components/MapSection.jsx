43a2d16c1ebc6d799f2742243f1ecf63c2286948
Thomas <thomas@craftacademy.se>
2021-06-13 00:14:53 +0200

    WIP small stylistic refactorings

--- src/components/MapSection.jsx
+++ src/components/MapSection.jsx
@@ -5,11 +5,13 @@ import {
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
 
 import { providers } from "../data/gothenburg";
 import { mapStyles } from "../data/mapStyles";
@@ -42,10 +44,14 @@ const MapSection = () => {
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_KEY,
   });
-  let markers = providers.map((provider) => {
+  const markers = providers.map((provider) => {
     return (
       <Marker
-        key={provider.name + provider.fullAddress.latitude + provider.fullAddress.longitude}
+        key={
+          provider.name +
+          provider.fullAddress.latitude +
+          provider.fullAddress.longitude
+        }
         icon={{
           path: "M256,0C167.641,0,96,71.625,96,160c0,24.75,5.625,48.219,15.672,69.125C112.234,230.313,256,512,256,512l142.594-279.375 C409.719,210.844,416,186.156,416,160C416,71.625,344.375,0,256,0z M256,256c-53.016,0-96-43-96-96s42.984-96,96-96 c53,0,96,43,96,96S309,256,256,256z",
           scale: 0.08,
@@ -74,37 +80,31 @@ const MapSection = () => {
       >
         {markers}
         {selected.fullAddress && (
-          <>
-            <InfoWindow
-              position={{
-                lat: selected.fullAddress.latitude,
-                lng: selected.fullAddress.longitude,
-              }}
-              clickable={true}
-              onCloseClick={() => setSelected({})}
-            >
-              <Card>
-                <CardHeader
-                  title={selected.name}
-                  subheader={`${selected.address}`}
-                />
-                <CardMedia
-                  style={{ height: 0, paddingTop: "36.25%" }}
-                  image={selected.images[0]}
-                  title={`${selected.name} office space`}
-                />
-                <CardContent>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {selected.description}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </InfoWindow>
-          </>
+          <InfoWindow
+            position={{
+              lat: selected.fullAddress.latitude,
+              lng: selected.fullAddress.longitude,
+            }}
+            clickable={true}
+            onCloseClick={() => setSelected({})}
+          >
+            <Card>
+              <CardHeader
+                title={selected.name}
+                subheader={`${selected.address}`}
+              />
+              <CardMedia
+                style={{ height: 0, paddingTop: "36.25%" }}
+                image={selected.images[0]}
+                title={`${selected.name} office space`}
+              />
+              <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {selected.description}
+                </Typography>
+              </CardContent>
+            </Card>
+          </InfoWindow>
         )}
       </GoogleMap>
     )
