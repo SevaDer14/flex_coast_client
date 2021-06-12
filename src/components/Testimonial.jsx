ed624d5e86fd0e5514f6536757d73db30a35c69f
sevader14 <vsevolod.deriushkin@gmail.com>
2021-06-12 16:19:05 +0200

    adds testimonial slideshow that stops if arrow is cklicked

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
-import Divider from '@material-ui/core/Divider'
 import { motion, AnimatePresence } from 'framer-motion'
 
-const TestimonialSlider = ({ slider, info, sliderHandler }) => {
+const TestimonialSlider = ({ slider, info, sliderHandler, stopTimer }) => {
   return (
     <AnimatePresence initial={false}>
       <motion.div
@@ -26,7 +25,11 @@ const TestimonialSlider = ({ slider, info, sliderHandler }) => {
               <p className='job-title'>{info.company}</p>
             </div>
             <div className='logo-section'>
-              <IconButton onClick={() => sliderHandler(-1)}>
+              <IconButton
+                onClick={() => {
+                  sliderHandler(-1)
+                  stopTimer()
+                }}>
                 <ChevronLeftIcon className='slider-button' fontSize='large' />
               </IconButton>
               <img
@@ -34,7 +37,11 @@ const TestimonialSlider = ({ slider, info, sliderHandler }) => {
                 src={info.logo}
                 alt='company-logo'
               />
-              <IconButton onClick={() => sliderHandler(1)}>
+              <IconButton
+                onClick={() => {
+                  sliderHandler(1)
+                  stopTimer()
+                }}>
                 <ChevronRightIcon className='slider-button' fontSize='large' />
               </IconButton>
             </div>
--- src/components/TestimonialsSection.jsx
+++ src/components/TestimonialsSection.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import Testimonial from './Testimonial'
-import illustration from '../assets/office-illu.png'
 import { testimonials } from '../assets/testimonials'
 
 const TestimonialsSection = () => {
   const [slider, setSlider] = useState(1)
+  const [timeoutActive, setTimeoutActive] = useState(true)
+
   const sliderHandler = (number) => {
     if (slider + number === 4) {
       setSlider(1)
@@ -12,34 +13,35 @@ const TestimonialsSection = () => {
       setSlider(3)
     } else {
       setSlider(slider + number)
-    }    
+    }
   }
 
-  //onMouseOver Stop Slinding (set State) & clear TimeOut on click
+  let slideShow = undefined
 
-  useEffect(() => {
-    setTimeout(() => {
+  const stopTimer = () => {
+    setTimeoutActive(false)
+    clearTimeout(slideShow)
+  }
+
+  const startTimer = () => {
+    slideShow = setTimeout(() => {
       sliderHandler(1)
     }, 4000)
-  }, [slider])
+  }
+
+  useEffect(() => {    
+    timeoutActive && startTimer()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [slider, timeoutActive])
 
   return (
     <>
-      <div className='illu-container'>
-        <div>
-          <img className='illustration' src={illustration} alt='illustration' />
-          <h4 className='testimonial-subtitle'>High end flexibility</h4>
-          <h1 className='testimonial-title'>
-            In the last 12 months, Flex Coast has helped over 100 companies find
-            their new offices - here are some examples
-          </h1>
-        </div>
-      </div>
       <div className='testimonials-container'>
         <Testimonial
           slider={slider}
           info={testimonials[slider - 1]}
           sliderHandler={sliderHandler}
+          stopTimer={stopTimer}
         />
       </div>
     </>
