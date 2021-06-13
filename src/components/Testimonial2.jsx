12659440315e1a534943c3dc5da6487a33ee8081
arunbhalli <tomarbhalli@gmail.com>
2021-06-13 18:38:57 +0200

    Adds swiperjs for styling uses swiperjs for animation added testimonial content adds infinite loop animation

--- src/components/Testimonial2.jsx
+++ src/components/Testimonial2.jsx
@@ -1,52 +1,104 @@
-import React from 'react'
 import profile1 from '../assets/social-img-1.jpg'
 import profile2 from '../assets/social-img-2.jpg'
 import profile3 from '../assets/social-img-3.jpg'
+import React, { useRef, useState } from 'react'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { EffectCoverflow } from 'swiper/core'
 
+import 'swiper/swiper.min.css'
+import 'swiper/components/effect-coverflow/effect-coverflow.min.css'
+import 'swiper/components/pagination/pagination.min.css'
+import 'swiper/components/navigation/navigation.min.css'
+import SwiperCore, { Pagination, Navigation } from 'swiper/core'
 const Testimonial2 = () => {
+  SwiperCore.use([EffectCoverflow, Pagination, Navigation])
   return (
-    <div className='items'>
-      <div className='card'>
-        <img className='profile-image' src={profile1} alt='profile-image' />
-        <div className='card-body'>
-          <h5 class='card-title'>Delbert Simonas</h5>
-          <p class='card-text'>
-            "Online reviews can make or break a customer's decision to make a
-            purchase. Read about these customer review on site"
-          </p>
-        </div>
-      </div>
-      <div className='card'>
-        <img className='profile-image' src={profile2} alt='profile-image' />
-        <div className='card-body'>
-          <h5 class='card-title'>Delbert Simonas</h5>
-          <p class='card-text'>
-            "Online reviews can make or break a customer's decision to make a
-            purchase. Read about these customer review on site"
-          </p>
-        </div>
-      </div>
-      <div className='card'>
-        <img className='profile-image' src={profile3} alt='profile-image' />
-        <div className='card-body'>
-          <h5 class='card-title'>Delbert Simonas</h5>
-          <p class='card-text'>
-            "Online reviews can make or break a customer's decision to make a
-            purchase. Read about these customer review on site"
-          </p>
-        </div>
-      </div>
-      <div className='card'>
-        <img className='profile-image' src={profile2} alt='profile-image' />
-        <div className='card-body'>
-          <h5 class='card-title'>Delbert Simonas</h5>
-          <p class='card-text'>
-            "Online reviews can make or break a customer's decision to make a
-            purchase. Read about these customer review on site"
-          </p>
-        </div>
-      </div>
-    </div>
+    <Swiper
+      effect={'coverflow'}
+      grabCursor={true}
+      centeredSlides={true}
+      slidesPerView={4}
+      navigation={true}
+      pagination={true}
+      loop={true}>
+      <section>
+        <SwiperSlide>
+          <div className='swiper-container'>
+            <div className='swiper-wrapper'>
+              <div className='swiper-slide'>
+                <div className='testimonialBox'>
+                  <div className='details'>
+                    <img
+                      className='profile-image'
+                      src={profile1}
+                      alt='profile-image'
+                    />
+                    <h3>Delbert Simonas</h3>
+                  </div>
+                  <div className='content'>
+                    <p>
+                      "Online reviews can make or break a customer's decision to
+                      make a purchase. Read about these customer review on site"
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='swiper-container'>
+            <div className='swiper-wrapper'>
+              <div className='swiper-slide'>
+                <div className='testimonialBox'>
+                  <div className='details'>
+                    <img
+                      className='profile-image'
+                      src={profile2}
+                      alt='profile-image'
+                    />
+                    <h3>Delbert Simonas</h3>
+                  </div>
+                  <h5>Delbert Simonas</h5>
+                  <div className='content'>
+                    <p>
+                      "Online reviews can make or break a customer's decision to
+                      make a purchase. Read about these customer review on site"
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='swiper-container'>
+            <div className='swiper-wrapper'>
+              <div className='swiper-slide'>
+                <div className='testimonialBox'>
+                  <div className='details'>
+                    <img
+                      className='profile-image'
+                      src={profile3}
+                      alt='profile-image'
+                    />
+                    <h3>Delbert Simonas</h3>
+                  </div>
+                  <div className='content'>
+                    <h5>Delbert Simonas</h5>
+                    <p>
+                      "Online reviews can make or break a customer's decision to
+                      make a purchase. Read about these customer review on site"
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </section>
+    </Swiper>
   )
 }
 export default Testimonial2
