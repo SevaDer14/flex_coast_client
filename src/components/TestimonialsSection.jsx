457d7fabc06a71a2ab1908c3c58ba45f83a743ad
luke <lukeperrin27@gmail.com>
2021-06-13 13:03:22 +0200

    fixes formatting issue

--- src/components/TestimonialsSection.jsx
+++ src/components/TestimonialsSection.jsx
@@ -29,7 +29,7 @@ const TestimonialsSection = () => {
     }, 4000)
   }
 
-  useEffect(() => {    
+  useEffect(() => {
     timeoutActive && startTimer()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [slider, timeoutActive])
