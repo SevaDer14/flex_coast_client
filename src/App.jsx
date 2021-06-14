1cb4f17e68f88071c7b26d90241cd3a0469c98b7
oliverochman <oliver.ochman@hotmail.com>
2021-06-14 16:49:28 +0200

    add AHOY and send track for site visit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import QuestionaryForm from './views/QuestionaryForm'
 import './styles/globals.css'
 import { Route, Switch } from 'react-router-dom'
 import LandingPage from './views/LandingPage'
+import ahoy from 'ahoy.js';
 import Footer from './components/Footer'
 
 const App = () => {
+
+  ahoy.configure({
+    urlPrefix: "http://localhost:3000/api",
+    visitsUrl: "/ahoy/visits",
+    eventsUrl: "/ahoy/events",
+  });
+  
+
+  useEffect(() => {
+    // what are we interested in
+    // language, IP address, coordinates, device type, platform
+    debugger
+    ahoy.track('Site visit'); 
+    ahoy.trackClicks();
+  }, [])
+
   return (
     <>
       <Switch>
