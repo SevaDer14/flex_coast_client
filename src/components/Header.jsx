7906e3737d57b9844e146530be1ae9a40e0b0e12
Christer <christerforsgren91@gmail.com>
2021-06-14 17:08:58 +0200

    Remove react router dom in index.jsx, header.jsx and custombutton.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
 import LanguageSelect from './LanguageSelect'
 import logo from '../assets/logo.svg'
 import CustomButton from './custom/CustomButton'
@@ -11,19 +10,12 @@ import Drawer from '@material-ui/core/Drawer'
 const LandingHeader = ({ landing }) => {
   const [open, setOpen] = useState(false)
   const isSmall = useMediaQuery('(max-width:620px)')
-  let history = useHistory()
 
   return (
     <div
       data-cy='header'
       className={landing ? 'page-header-landing' : 'page-header'}>
-      <img
-        src={logo}
-        alt='logo'
-        className='logo'
-        data-cy='flex-coast'
-        onClick={() => history.push('/')}
-      />
+      <img src={logo} alt='logo' className='logo' data-cy='flex-coast' />
       {isSmall ? (
         <div className='dropdown-menu'>
           <IconButton onClick={() => setOpen(true)}>
@@ -38,7 +30,7 @@ const LandingHeader = ({ landing }) => {
               <CustomButton dataCy='phone-number'>
                 <a href='tel:+46812345678'>08-123 456 78</a>
               </CustomButton>
-              <LanguageSelect setOpen={()=>setOpen()}/>
+              <LanguageSelect setOpen={() => setOpen()} />
             </>
           </Drawer>
         </div>
