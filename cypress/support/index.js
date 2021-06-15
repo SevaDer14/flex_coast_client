c8ef1e75a953397120456be335cf2976406efa4b
arunbhalli <tomarbhalli@gmail.com>
2021-06-15 21:38:18 +0200

    adds color to and fixed test for translation

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,7 +19,7 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-before(()=> {
+beforeEach(()=> {
   cy.intercept('**/api/ahoy/visits', {})
   cy.intercept('**/api/ahoy/events', {}).as('eventRequest')
 })
