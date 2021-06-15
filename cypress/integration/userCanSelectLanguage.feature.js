900b0a80d2391916619c99864daa6709f94df80c
kim_haaga <kim.haaga@live.se>
2021-06-15 09:49:52 +0200

    revrite test to not go to /inquiry

--- cypress/integration/userCanSelectLanguage.feature.js
+++ cypress/integration/userCanSelectLanguage.feature.js
@@ -1,20 +1,20 @@
 describe('User can switch between languages', () => {
   beforeEach(() => {
-    cy.visit('/inquiry')
+    cy.visit('/')
   })
 
   it('is expected to change the UI language', () => {
     cy.get('[data-cy=language-dropdown]').click()
     cy.get('[data-cy=svenska]').click()
-    cy.get('[data-cy=welcome-message]').should(
+    cy.get('[data-cy=welcome-text]').should(
       'contain',
-      'Hej! För att att hitta bästa möjliga kontor, behöver vi ställa lite frågor.'
+      'Flexibla kontor i vackra Göteborg'
     )
     cy.get('[data-cy=language-dropdown]').click()
     cy.get('[data-cy=english]').click()
-    cy.get('[data-cy=welcome-message]').should(
+    cy.get('[data-cy=welcome-text]').should(
       'contain',
-      "Hi! Let's talk requirements, so we can provide personalised suggestions for your company."
+      "Flexible office spaces in beautiful Gothenburg"
     )
   })
 })
