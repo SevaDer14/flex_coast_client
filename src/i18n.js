3ac90a58d508808acddefd757e9d3777f8e33106
DavDan1 <david-danielyan@hotmail.com>
2021-06-16 17:07:56 +0200

    removes unused lines from i18n file

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,5 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
 import en from './locales/en'
 import se from './locales/se'
 
@@ -23,7 +22,6 @@ const options = {
 
 i18n
   .use(initReactI18next)
-  .use(LanguageDetector)
   .init({
     detection: options,
     resources,
