1986b93b7f7acfb94b0f9ff58a82fe7282984380
sevader14 <vsevolod.deriushkin@gmail.com>
2021-06-18 13:52:08 +0200

    tries to fix by edit html meta tag

--- src/modules/analytics.js
+++ src/modules/analytics.js
@@ -2,7 +2,7 @@ import ahoy from 'ahoy.js'
 
 const apiUrl =
   process.env.REACT_APP_STAGE === 'production'
-    ? 'https://flex-coast-production.herokuapp.com/api/'
+    ? process.env.REACT_APP_API_URL
     : 'https://flex-coast-api-development.herokuapp.com/api/'
 // const apiUrl = 'http://localhost:3000/api/'
 
