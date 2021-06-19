cdfeba2ede43fff9c00baa3590833f7ce69970a8
luke <lukeperrin27@gmail.com>
2021-06-19 11:13:50 +0200

    refactors api call

--- src/modules/analytics.js
+++ src/modules/analytics.js
@@ -1,10 +1,6 @@
 import ahoy from 'ahoy.js'
 
-const apiUrl =
-  process.env.REACT_APP_STAGE === 'production'
-    ? process.env.REACT_APP_API_URL
-    : 'https://flex-coast-api-development.herokuapp.com/api/'
-// const apiUrl = 'http://localhost:3000/api/'
+const apiUrl = process.env.REACT_APP_API_URL
 
 ahoy.configure({
   urlPrefix: apiUrl,
